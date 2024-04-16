c3381f644947c528cc1655fe71272d6834052a8e
LuBajer <LuBajer@cpas.cz>
2024-04-16 15:16:44 +0200

    implementace cvičení č. 7

---
+++ src/initData.js
@@ -0,0 +1,18 @@
+export const initData = [
+    {
+      title: 'Virtual DOM',
+      description: 'Virtual DOM je koncept, který React používá pro efektivní manipulaci s DOM.',
+    },
+    {
+      title: 'Komponenty',
+      description: 'Komponenty jsou základní stavební jednotkou React aplikace.',
+    },
+    {
+      title: 'Stav (State)',
+      description: 'Stav je datová struktura, která určuje chování a vzhled komponenty.',
+    },
+    {
+      title: 'Props',
+      description: 'Props jsou vstupní parametry komponenty, které určují její chování a vzhled.',
+    },
+  ];
